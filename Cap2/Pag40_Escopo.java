d0b3839b68aeffd00248ab379c367f59fc3e1945
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-04-22 19:42:50 -0300

    pág. 40

---
+++ Cap2/Pag40_Escopo.java
@@ -0,0 +1,24 @@
+public class Pag40_Escopo { // Início do bloco 0
+	public static void main (String arg[]) { // Início do bloco 1
+		int i = 5; // Variável do bloco 1
+		{ // Início do bloco 2
+			int j = 0; // Variável do bloco 2
+			System.out.println(i);	// Uso de i dentro do seu escopo
+			j = 5 * i;					// Uso de j dentro do seu escopo
+			System.out.println(j);
+			{ // Inicio do bloco 3
+				System.out.println(i);	// Uso de i dentro do seu escopo
+				System.out.println(j);	// Uso de j dentro do seu escopo
+			}
+		}
+		System.out.println(i);	// Uso de i dentro do seu escopo
+//→→→→→	System.out.println(j); ←←←←←// ERRO! Uso de j FORA do seu escopo
+	}
+}
+/*
+	"OS DE DENTRO VEEM OS DE FORA"
+ 	•escopos externos são visíveis nos escopos internos
+
+	"OS DE FORA NÃO VEEM OS DE DENTRO"
+ 	•escopos internos são invisíveis nos escopos externos
+*/
