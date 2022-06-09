c1967350cfbc88b341be9eef2d0293002ab6dadc
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-06-09 20:12:38 -0300

    pág. 50

---
+++ Cap2/Pag50_OperadoresAritmeticos.java
@@ -0,0 +1,30 @@
+public class Pag50_OperadoresAritmeticos {
+	public static void main (String args[]) {
+		
+		int a = 5, b = 2; // declara e inicia variáveis
+		System.out.println("Valores: a = " + a + ", b = " + b); 
+		System.out.println(" -b = " + (-b)); // operações aritméticas
+		System.out.println("a + b = " + (a + b)); 
+		System.out.println("a - b = " + (a - b));
+		System.out.println("a * b = " + (a * b));
+		System.out.println("a / b = " + (a / b));
+		System.out.println("(float) a / b = " + ((float)a / b));
+		System.out.println("a % b = " + (a % b));
+		System.out.println("a++ = " + (a++));
+		System.out.println("  b = " + ( b));
+		System.out.println("Valores: a = " + a + ", b = " + b);
+
+		
+		//VER FIGURA DOS TIPOS DE DADOS NA PÁGINA 50.
+
+		//Promoção de tipos: tipo simples é convertido em tipo complexo
+		short s = 3;
+		int i = s; //s é promovido a inteiro
+		System.out.println("Promoção de tipos: " + i);
+
+		//Coerção de tipos (type casting): tipo complexo é convertido em tipo simples
+		double d = 6.47;
+		int x = (int) d; //a parte fracionária de d é truncada, x recebe d convertido em um tipo mais simples
+		System.out.println("Coerção de tipos (type casting): " + x);
+	}
+}
