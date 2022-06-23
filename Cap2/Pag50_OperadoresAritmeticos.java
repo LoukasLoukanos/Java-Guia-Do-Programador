aca6efc535f0be305a1bb34d80180a0606430bcc
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-06-23 18:54:44 -0300

    pág. 50

--- Cap2/Pag50_OperadoresAritmeticos.java
+++ Cap2/Pag50_OperadoresAritmeticos.java
@@ -1,47 +1,43 @@
 public class Pag50_OperadoresAritmeticos {
-	public static void main(String args[]) {
-		/*
-		 * ______________________________________________________________________________
-		 * OPERADORES ARITMÉTICOS
-		 * -----------------------------------------------------------------------------
-		 * -
-		 * OPERADOR SIGNIFICADO ASSOCIATIVIDADE EXEMPLO
-		 * + Adição Esquerda a + b
-		 * - Subtração Esquerda a - b
-		 * * Multiplicação Esquerda a * b
-		 * / Divisão Esquerda a / b
-		 * % Resto da Divisão inteira Esquerda a % b
-		 * - Sinal negativo (- unário) Direita -a
-		 * + Sinal positivo (+ unário) Direita +a
-		 * ++ Incremento unitário Esquerda/Direita ++a ou a++
-		 * -- Decremento unitário Esquerda/Direita --a ou a--
-		 * ______________________________________________________________________________
-		 */
+	public static void main (String args[]) {
+		/*|———————————————————————————————————————————————————————————————————————————————————|
+		 *|______________________________OPERADORES ARITMÉTICOS_______________________________|
+		 *|___OPERADOR___|________SIGNIFICADO_________|___ASSOCIATIVIDADE____|____EXEMPLO_____|
+		 *|______+_______|_Adição_____________________|_____Esquerda_________|_____a + b______|
+		 *|______-_______|_Subtração__________________|_____Esquerda_________|_____a - b______|
+		 *|______*_______|_Multiplicação______________|_____Esquerda_________|_____a * b______|
+		 *|______/_______|_Divisão____________________|_____Esquerda_________|_____a / b______|
+		 *|______%_______|_Resto da Divisão inteira___|_____Esquerda_________|_____a % b______|
+		 *|______-_______|_Sinal negativo (- unário)__|_____Direita__________|_______-a_______|
+		 *|______+_______|_Sinal positivo (+ unário)__|_____Direita__________|_______+a_______|
+		 *|______++______|_Incremento unitário________|_____Esquerda/Direita_|___++a ou a++___|
+		 *|______--______|_Decremento unitário________|_____Esquerda/Direita_|___--a ou a--___|
+		 *|———————————————————————————————————————————————————————————————————————————————————|*/
+
 
 		int a = 5, b = 2; // declara e inicia variáveis
-		System.out.println("Valores: a = " + a + ", b = " + b);
+		System.out.println("Valores: a = " + a + ", b = " + b); 
 		System.out.println(" -b = " + (-b)); // operações aritméticas
-		System.out.println("a + b = " + (a + b));
+		System.out.println("a + b = " + (a + b)); 
 		System.out.println("a - b = " + (a - b));
 		System.out.println("a * b = " + (a * b));
 		System.out.println("a / b = " + (a / b));
-		System.out.println("(float) a / b = " + ((float) a / b));
+		System.out.println("(float) a / b = " + ((float)a / b));
 		System.out.println("a % b = " + (a % b));
 		System.out.println("a++ = " + (a++));
-		System.out.println("  b = " + (b));
+		System.out.println("  b = " + ( b));
 		System.out.println("Valores: a = " + a + ", b = " + b);
 
-		// VER FIGURA DOS TIPOS DE DADOS NA PÁGINA 50.
+		//VER FIGURA DOS TIPOS DE DADOS NA PÁGINA 50.
 
-		// Promoção de tipos: tipo simples é convertido em tipo complexo
+		//Promoção de tipos: tipo simples é convertido em tipo complexo
 		short s = 3;
-		int i = s; // s é promovido a inteiro
+		int i = s; //s é promovido a inteiro
 		System.out.println("Promoção de tipos: " + i);
 
-		// Coerção de tipos (type casting): tipo complexo é convertido em tipo simples
+		//Coerção de tipos (type casting): tipo complexo é convertido em tipo simples
 		double d = 6.47;
-		int x = (int) d; // a parte fracionária de d é truncada, x recebe d convertido em um tipo mais
-							// simples
+		int x = (int) d; //a parte fracionária de d é truncada, x recebe d convertido em um tipo mais simples
 		System.out.println("Coerção de tipos (type casting): " + x);
 	}
 }
