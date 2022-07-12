2c13aad4c930272587294ab41bf55110db520c84
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-07-12 20:55:50 -0300

    pág. 73

--- Cap2/Pag72_Diretiva_break.java
+++ Cap2/Pag72_Diretiva_break.java
@@ -17,7 +17,7 @@ public class Pag72_Diretiva_break {
 		/* 5°-Estrutura de Desvio de Fluxo Incondicional: diretiva (Statement) break_______________________________________________________________
 		 * A diretiva break provoca a interrupção em estrutura de repetição for, while e do while e estrutura de desvio switch */
 		
-		// Exemplo: Diretiva(Statement) em for:
+		// Exemplo: Diretiva(Statement) break em for:
 		// leitura do número máximo de pares desejados
 		System.out.print("Maximo no pares ? ");
 		Scanner s = new Scanner(System.in); // Prepara entrada de dados criando o objeto leitor s para uso das ENTRADAS FORMATADAS (Pag45_EntradaFormatada)
@@ -31,4 +31,22 @@ public class Pag72_Diretiva_break {
 			if (pares==max) { break; } // break provoca a interrupção
 		}
 		s.close(); // Fecha objeto leitor s, evitando a perda de recursos e impedindo o uso posterior do dispositivo de entrada
+
+		
+		/* 5°-Estrutura de Desvio de Fluxo Incondicional: diretiva (Statement) continue____________________________________________________________
+		 * A diretiva break provoca a interrupção em estrutura de repetição for, while e do while, fazendo com que a
+		 * estrutura de repetição continue direto para a próxima iteração, pulando as diretivas seguintes finais...*/
+		
+		// Exemplo: Diretiva(Statement) continue em for:
+		int soma = 0; // variável para armazenar soma
+		for (int i=0; i<=100; i++) { // laco de 0 a 100
+			// abrevia iteração se múltiplo de 2, 3 ou 5
+			if (i%2==0 || i%3==0 || i%5==0) { //se o contador é multiplo se 2, 3 ou 5
+				System.out.println("i = "+ i + " multiplo de 2, 3 ou 5");
+				continue; // provoca a interrupção da estrutura de repetição e continua direto para a próxima iteração
+			}
+			// processamento normal para demais casos, se if não for True
+			soma = soma + i;
+			System.out.println("i = " + i + "  soma = "+ soma);
+		}
 } }
