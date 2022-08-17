316a61eef1bd0e56254dda8827e151f3db88f94f
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-08-16 21:13:34 -0300

    pág. 94

--- Cap2/Pag94_AnotacoesJava.java
+++ Cap2/Pag94_AnotacoesJava.java
@@ -3,13 +3,17 @@ public class Pag94_AnotacoesJava {
 		System.out.println("Valor aleatorio: " + Anotacoes.random());
 	}
 }
-
+// As anotações são meetainformações incluída no código do programa, com o objetivo de fornecer indicações para 
+// ferramentas que vão manipulá-lo, ou seja, servem como diretivas para compiladores, IDEs ou outros programas.
 class Anotacoes {
-	@Deprecated
+	@Deprecated //marca o método anotado como absoleto, isto é, que não deveria ser utilizado
 	public static double random() { return Math.random(); }
-	 
-	@Override
-	@SuppressWarnings("unused")
+	
+	@Override // indica que o método anotado sobrepõe outro existente em sua superclasse (o compilador gera erro casso não ocorra a sobreposição)
+	// •Override(Sobreposição de Métodos):reescrever o método herdado da superclasse com a mesma assinatura, alterando o corpo do método
+    // •Overload(Sobrecarga de Métodos):escrever vários métodos com o mesmo nome, com parâmetros e assinaturas(identificação) diferentes
+	
+	@SuppressWarnings("unused") // ordena que o compilador suprima os avisos gerados por um elemento (classe, método ou diretiva) e seus subelementos.
 	public String toString() {
 		int x;
 		return "ExemploAnotação";
