547ae9910cb964d1d7af63acf7eb06f981109d37
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-09-07 21:05:42 -0300

    pág. 106

--- Cap3/Pag103a113_Metodos.java
+++ Cap3/Pag103a113_Metodos.java
@@ -7,6 +7,7 @@
  * • métodos de destruição: são responsáveis pela remoção de um objeto da memória. */
 
  public class Pag103a113_Metodos {
+	//___SIMPLES_____________________________________________
 	public int hor, min, seg;
 	public void setHorario(int h, int m, int s) {
 		hor = h;
@@ -15,5 +16,39 @@
 	}
 	public String toString() {
 		return hor + ":" + min + ":" + seg;
+	}
+
+    //___MELHORADA___________________________________________
+	// três atributos int de acesso restrito
+	private int hora, minu, segu; 
+	// métodos de acesso (leitura) dos campos restritos
+	public int getHoras () {
+		return hora;
+	}
+	public int getMinutos () {
+		return minu;
+	}
+	public int getSegundos () { 
+		return segu;
+	}
+	// métodos de alteração (escrita) dos campos restritos
+	public void setHoras (int h) {
+		if(h>=0 && h<24) hor = h;
+	}
+	public void setMinutos (int m) {
+		if(m>=0 && m<60) min = m;
+	}
+	public void setSegundos (int s) {
+		if(s>=0 && s<60) seg = s;
+	}
+	public void setHorari (int h, int m, int s) {
+		setHoras(h);
+		setMinutos(m);
+		setSegundos(s);
+	}
+	// representação textual dos objetos Hora
+	public String toString_ () {
+		return String.format("%02d:%02d:%02d",
+			getHoras(), getMinutos(), getSegundos());
 	}
 }
