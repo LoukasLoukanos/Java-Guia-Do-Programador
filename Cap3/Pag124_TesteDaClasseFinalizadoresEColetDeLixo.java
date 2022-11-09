43e1cb3b38de60d1eb244a85acfe451c2efe5490
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2022-11-08 21:03:29 -0300

    Pág. 124

--- Cap3/Pag124_TesteDaClasseFinalizadoresEColetDeLixo.java
+++ Cap3/Pag124_TesteDaClasseFinalizadoresEColetDeLixo.java
@@ -3,6 +3,7 @@ public class Pag124_TesteDaClasseFinalizadoresEColetDeLixo {
 		System.out.println("Instancias = "+ Pag123_FinalizadoresEColetaDeLixo.getInstancias());
 		for (int i=0; i<10; i++) {
 			Pag123_FinalizadoresEColetaDeLixo o = new Pag123_FinalizadoresEColetaDeLixo ();
+			System.out.println(o);
 		}
 		System.out.println("Instancias = "+ Pag123_FinalizadoresEColetaDeLixo.getInstancias());
 		System.gc(); // aciona garbage collector
