39a75d591db05e0a9e467e3b3c6b10d937e0d88f
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-01-19 20:45:52 -0300

    Pág. 147 à 151

--- Cap4/Pag147a151_HerancaeSobreposicao.java
+++ Cap4/Pag147a151_HerancaeSobreposicao.java
@@ -19,21 +19,47 @@ public class Pag147a151_HerancaeSobreposicao {
 	public String toString ( ) {
 		return "Pag147a151_HerancaeSobreposicao[kProp=" +kProp+",kLin= " + kLin+"]";
 	}
-	
+
+public class Aplicacao {
+	public static void main(String[] args) {
+		// Um conversor de medidas em centímetros para polegadas poderia ser definido, instanciado e usado assim (com o jshell omitindo os comentários):
+
+		//open Pag147a151_HerancaeSobreposicao.java   →   Carrega arquivo com código Java
+
+		// instancia conversor com valores adequados
+		Pag147a151_HerancaeSobreposicao c2p = new Pag147a151_HerancaeSobreposicao (0.3937, 0.0);
+
+		// medida en cm
+		double cm = 15;
+
+		// conversão cn para pol
+		double pol = c2p.converter (cm);
+
+		//vars   →   exibe variáveis e seus valores
+	}
+}
 
 
-	// Um conversor de medidas em centímetros para polegadas poderia ser definido, instanciado e usado assim (com o jshell omitindo os comentários):
-	
-	//open Pag147a151_HerancaeSobreposicao.java   →   Carrega arquivo com código Java
 
-	// instancia conversor com valores adequados
-	Pag147a151_HerancaeSobreposicao c2p = new Pag147a151_HerancaeSobreposicao (0.3937, 0.0);
-	
-	// medida en cm
-	double cm = 15;
-	
-	// conversão cn para pol
-	double pol = c2p.converter (cm);
-	
-	//vars   →   exibe variáveis e seus valores
+// Outros conversores podem ser criados como no exemplo acima↑, porém a infraestrutura da classe Pag147a151_HerancaeSobreposicao
+// é aproveitada melhor na implementação de conversores específicos por meio da herança :
+public class ArquivoExemplo_1 extends Pag147a151_HerancaeSobreposicao {
+	public ArquivoExemplo_1( ) {
+		// aciona o construtor da superclasse Pag147a151_HerancaeSobreposicao
+		super(0.3937, 0.0);
+	}
+}
+
+public class ArquivoExemplo_2 extends Pag147a151_HerancaeSobreposicao {
+	public ArquivoExemplo_2 ( ) {
+		// aciona o construtor da superclasse Pag147a151_HerancaeSobreposicao
+		super(1.0, 273.0); 
+	}
+}
+
+public class ArquivoExemplo_3 extends Pag147a151_HerancaeSobreposicao {
+	public ArquivoExemplo_3 ( ) {
+		// aciona o construtor da superclasse Pag147a151_HerancaeSobreposicao
+		super(1.0, -273.0); 
+	}
 }
