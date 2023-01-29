770802691f457ddaac26e90997cd7cd3db51d0df
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-01-28 22:27:08 -0300

    Pág. 154 e 171

--- Cap4/Pag154a171/jandl/jgp4/geom/P0408Circunferencia.java
+++ Cap4/Pag154a171/jandl/jgp4/geom/P0408Circunferencia.java
@@ -2,20 +2,20 @@ package jandl.jgp4.geom;
 
 import static java.lang.Math.*;
 
-public class P0408Circunferencia extends P0407Forma {
+public class P0408Circunferencia extends P0407Forma { // declarar a classe como abstract é redundante → já foi declarado na classe pai
 
 	public P0408Circunferencia (double raio) {
-		super(1); // uso obrigatório do construtor da superclasse
-		setRaio(raio); // ajuste da medida do raio
+		super(1); // uso obrigatório do construtor da superclasse (pai) P0407Forma
+		setRaio(raio); // ajuste da medida do raio pelo construtor (inicializador)
 	}
 
-	// implementação do método abstrato area()
-	@Override
+	// implementação do método abstrato area(). Sensato, pois o cálculo da área depende da forma específica
+	@Override // indicando a sobreposição do método abstrato ↓
 	public double area () {
 		return PI*Math.pow(getMedida(0), 2);
 	}
 
-	// novo método para ajuste do raio
+	// reajuste da medida do raio após a definição inicializadora do construtor P0408Circunferencia (double)
 	public void setRaio(double raio) {
 		setMedida(0, raio);
 	}
