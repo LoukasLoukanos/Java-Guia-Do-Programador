253572965a01d7715f8cd8eb7fb66f8fe0b3b529
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-02-01 19:30:14 -0300

    Pág. 154 e 171

--- Cap4/Pag154a171/jandl/jgp4/engine/P0411Imprimivel.java
+++ Cap4/Pag154a171/jandl/jgp4/engine/P0411Imprimivel.java
@@ -1,8 +1,11 @@
 package jandl.jgp4.engine;
 
 public interface P0411Imprimivel {
-	public static final String INICIO = "<inicio>";
-	public static final String FIM = "<fim>";
-	public abstract void imprimir ();
-	public abstract void imprimirNoConsole ();
+	public static final String INICIO = "<inicio>"; // declarar as constantes de interfaces como public, static e final é redundante ↓
+	String MEIO = "<meio>"; // constante da interface declarada implicitamente como public, static e final
+	public static final String FIM = "<fim>"; // redundante
+	
+	public abstract void imprimir (); // declarar os métodos de interfaces como public e abstract é redundante ↓
+	void imprimirFinalizar (); // método da interface declarada implicitamente como public e abstract
+	public abstract void imprimirNoConsole (); // redundante
 }
--- Cap4/Pag154a171/jandl/jgp4/engine/P0412Editavel.java
+++ Cap4/Pag154a171/jandl/jgp4/engine/P0412Editavel.java
@@ -1,10 +1,9 @@
 package jandl.jgp4.engine;
 
 public interface P0412Editavel {
-	long chave = 0xABDCEF0; // hexadecimal
+	long chave = 0xABDCEF0; // constante da interface declarada implicitamente como public, static e final
 	
-	// método abstrato
-	void editar(String conteudo);
+	void editar(String conteudo); // método da interface declarada implicitamente como public e abstract
 	
 	// método default (alteração exemplo 4.17)
 	default void limpar () {
