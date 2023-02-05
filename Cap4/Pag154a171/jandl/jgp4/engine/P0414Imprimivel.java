1ff4478d11aba8dd7bb79655c568dde6911bfbc7
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-02-05 18:54:52 -0300

    Pág. 154 e 171

--- Cap4/Pag154a171/jandl/jgp4/engine/P0414Imprimivel.java
+++ Cap4/Pag154a171/jandl/jgp4/engine/P0414Imprimivel.java
@@ -1,16 +1,21 @@
 package jandl.jgp4.engine;
+// → Classes podem herdar de apenas uma superclasse, mas podem implementar diversas interfaces (semelhante à herança múltipla)
 
+// herda da classe pai P0413Mensagem e implementa a interface P0411Imprimivel
 public class P0414Imprimivel extends P0413Mensagem implements P0411Imprimivel {
 			
+	// herança da classe pai —super—, P0413Mensagem
 	public P0414Imprimivel (String conteudo) {
 		super(conteudo);
 	}
 		
+	// implementação da interface P0411Imprimivel
 	@Override
 	public void imprimir () {
 		imprimirNoConsole();
 	}
 		
+	// implementação da interface P0411Imprimivel
 	@Override
 	public void imprimirNoConsole (){
 		System.out.println(P0411Imprimivel.INICIO);
--- Cap4/Pag154a171/jandl/jgp4/engine/P0415Editavel.java
+++ Cap4/Pag154a171/jandl/jgp4/engine/P0415Editavel.java
@@ -1,23 +1,27 @@
 package jandl.jgp4.engine;
+// → Classes podem herdar de apenas uma superclasse, mas podem implementar diversas interfaces (semelhante à herança múltipla)
 
-public class P0415Editavel extends P0413Mensagem 
-		implements P0411Imprimivel, P0412Editavel {
+// herda da classe pai P0413Mensagem e implementa as interfaces P0411Imprimivel e P0412Editavel, simulando o mecanismo de herança múltipla.
+public class P0415Editavel extends P0413Mensagem implements P0411Imprimivel, P0412Editavel {
 
+	// herança da classe pai —super—, P0413Mensagem
 	public P0415Editavel (String conteudo) {
 		super(conteudo);
 	}
 
+	// implementação da interface P0411Imprimivel
 	@Override
 	public void imprimir () {
 		imprimirNoConsole();
 	}
 
+	// implementação da interface P0411Imprimivel
 	@Override
 	public void imprimirNoConsole (){
-		System.out.println(P0411Imprimivel.INICIO + getConteudo()
-				+ P0411Imprimivel.FIM);
+		System.out.println(P0411Imprimivel.INICIO + getConteudo() + P0411Imprimivel.FIM);
 	}
 
+	// implementação da interface P0412Editavel
 	@Override
 	public void editar (String conteudo) {
 		setConteudo(conteudo);
