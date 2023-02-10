14ad75aed8c6160e8f11927c7f30108c3a51edd7
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-02-10 18:51:25 -0300

    Pág. 154 e 171

--- Cap4/Pag154a171/jandl/jgp4/engine/P0415Editavel.java
+++ Cap4/Pag154a171/jandl/jgp4/engine/P0415Editavel.java
@@ -1,5 +1,11 @@
 package jandl.jgp4.engine;
-// → Classes podem herdar de apenas uma superclasse, mas podem implementar diversas interfaces (semelhante à herança múltipla)
+/* → Classes podem herdar de apenas uma superclasse, mas podem implementar diversas interfaces (semelhante à herança múltipla).
+Obs: "Realizar" e "implementar" são termos usados de forma intercambiável para se referir ao mesmo conceito na programação. 
+No contexto da programação, ambos os termos se referem à ação de uma classe implementar as definições de uma interface, 
+fornecendo implementações concretas para seus métodos e propriedades.No entanto, "implementar" é um termo mais comumente usado na 
+programação orientada a objetos, enquanto "realizar" é mais comumente usado em linguagens de programação orientadas a protocolos, 
+como Swift. Ambas as palavras são sinônimos no contexto de programação e podem ser usadas para se referir à mesma ação.*/
+
 
 // herda da classe pai P0413Mensagem e implementa as interfaces P0411Imprimivel e P0412Editavel, simulando o mecanismo de herança múltipla.
 public class P0415Editavel extends P0413Mensagem implements P0411Imprimivel, P0412Editavel {
--- Cap4/Pag154a171/jandl/jgp4/engine/P0421Editavel2.java
+++ Cap4/Pag154a171/jandl/jgp4/engine/P0421Editavel2.java
@@ -1,9 +1,23 @@
 package jandl.jgp4.engine;
 
-public class P0421Editavel2 extends P0415Editavel
-		implements P0420Especial {
+// herda da classe pai P0415Editavel e implementa a interface d emarcação P0420Especial.
+public class P0421Editavel2 extends P0415Editavel implements P0420Especial {
 
+	// herança da classe pai —super—, P0415Editavel, que por sua vez recebe a herança da classe pai —super—, P0413Mensagem
 	public P0421Editavel2 (String conteudo) {
 		super(conteudo);
+	}
+
+	/* esta classe implementa uma interface de marcação (P0420Especial) portanto ela deve seguir as regras ou contratos associados a esta interface. Mesmo que a interface seja de marcação (não tenha nenhum método ou constante definido nela) */
+
+	public void operadorEspecialInstanceof () {
+
+		Object obj = new P0421Editavel2("Java 16");
+
+		/*o operador instanceof permite identificaar se objetos são instâncias de classes específicas ou se realizam interfaces particulares*/
+		System.out.println(obj instanceof P0421Editavel2); // true
+		System.out.println(obj instanceof String); // false
+		System.out.println(obj instanceof P0420Especial); // true
+	
 	}
 }
