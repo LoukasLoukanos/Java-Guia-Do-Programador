e6f408ee4e78e711d95bc3e9ef0f8d9f144a919c
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-03-06 21:27:26 -0300

    Pág. 154 a 171

--- Cap4/Pag154a171/jandl/jgp4/engine/P0420Especial.java
+++ Cap4/Pag154a171/jandl/jgp4/engine/P0420Especial.java
@@ -3,17 +3,32 @@ package jandl.jgp4.engine;
 public interface P0420Especial {
 	/* → INTERFACE DE MARCAÇÃO ← 
 	
-	As interfaces de marcação em Java não exigem a presença de métodos ou constantes. Elas podem ser criadas sem a presença de nenhum elemento.
+	As interfaces de marcação em Java não exigem a presença de métodos ou constantes. 
+	Elas podem ser criadas sem a presença de nenhum elemento.
 
-	Interfaces de marcação são usadas como um mecanismo para aplicar uma "marca" ou "rótulo" a uma classe, indicando que ela segue certas regras ou contratos. Por exemplo, uma classe que implementa a interface Serializable pode ser marcada como sendo passível de serialização.
+	Interfaces de marcação são usadas como um mecanismo para aplicar uma "marca" ou 
+	"rótulo" a uma classe, indicando que ela segue certas regras ou contratos. Por 
+	exemplo, uma classe que implementa a interface Serializable pode ser marcada como 
+	sendo passível de serialização.
 
-	Em resumo, uma interface de marcação pode ser criada sem a presença de métodos ou constantes, e sua principal função é marcar ou rotular as classes que a implementam.
+	Em resumo, uma interface de marcação pode ser criada sem a presença de métodos 
+	ou constantes, e sua principal função é marcar ou rotular as classes que a implementam.
 
-	As interfaces de marcação em Java são consideradas "vazias" no sentido de que elas não têm a obrigação de ter métodos ou constantes definidos nelas. No entanto, elas ainda são contratos ou regras que devem ser seguidas pelas classes que as implementam.
+	As interfaces de marcação em Java são consideradas "vazias" no sentido de que elas 
+	não têm a obrigação de ter métodos ou constantes definidos nelas. No entanto, elas 
+	ainda são contratos ou regras que devem ser seguidas pelas classes que as implementam.
 
-	Quando uma classe implementa uma interface de marcação, ela está declarando que segue as regras ou contratos associados àquela interface. Mesmo que a interface não tenha nenhum método ou constante definido nela, ela ainda é um contrato que a classe que a implementa deve seguir.
+	Quando uma classe implementa uma interface de marcação, ela está declarando que segue 
+	as regras ou contratos associados àquela interface. Mesmo que a interface não tenha 
+	nenhum método ou constante definido nela, ela ainda é um contrato que a classe que a 
+	implementa deve seguir.
 
-	Por exemplo, uma classe que implementa a interface Serializable está declarando que é passível de serialização, mesmo que a interface Serializable não tenha nenhum método ou constante definido nela. A classe que implementa a interface está seguindo o contrato associado à interface de ser passível de serialização.
+	Por exemplo, uma classe que implementa a interface Serializable está declarando que 
+	é passível de serialização, mesmo que a interface Serializable não tenha nenhum método 
+	ou constante definido nela. A classe que implementa a interface está seguindo o contrato 
+	associado à interface de ser passível de serialização.
 
-	Em resumo, mesmo que as interfaces de marcação não tenham métodos ou constantes definidos nelas, elas ainda são consideradas contratos ou regras que as classes que as implementam devem seguir. */
+	Em resumo, mesmo que as interfaces de marcação não tenham métodos ou constantes 
+	definidos nelas, elas ainda são consideradas contratos ou regras que as classes 
+	que as implementam devem seguir. */
 }
