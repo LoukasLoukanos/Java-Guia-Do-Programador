e5a87f595ed3411ef005e4587915c5d64f6cc1f4
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-04-12 19:17:02 -0300

    177 à 179

--- Cap4/Pag177a179_ClassesWrapper.java
+++ Cap4/Pag177a179_ClassesWrapper.java
@@ -51,108 +51,48 @@ public class Pag177a179_ClassesWrapper {
 ╚═════════════════╩════════════════════╩════════════════════╩═════════════════╝
 
 
-_____________________________________________________________________________________________________________________________
-Como qualquer classe Java todas as classes wrapper oferecem os métodos públicos disponiveis em object, dos quais se destacam:
+/*_______________________________________________________________________________________________________________________________________
+ * Como qualquer classe Java, todas as classes wrapper oferecem os métodos públicos disponíveis em Object, dos quais se destacam:
+ * • boolean equals(Object) → para realização de comparações;
+ * • String toString() → que fornece uma representação textual do objeto, no caso, seu valor expresso como uma string (um objeto String).
+ 
+ * As classes derivadas de Number, como Integer e Double, oferecem ainda outros métodos:
+ * • int compareTo(Object) → que realiza comparações entre objetos das classes wrapper
+ * • byte byteValue(), • double doubleValue(), • float floatValue(), • int intValue(), • long longValue() e • short shortValue():
+ *   → retornam o valor encapsulado como um tipo primitivo */
 
-• boolean equals (Cbject) → para realização de comparações;
-
-• String toString() → que fornece uma representação textual do objeto, no caso, seu valor expresso como uma string (um objeto String). 
-
-
-_____________________________________________________________________________________
-As classes derivadas de Number, como Integer e Double, oferecem ainda outros métodos:
-
-• byte txtevalar(), • double dalevalar(), • float floatvalue(), • int intvalue(), • Long LongValue() e • short shortValue():
-    → retornam o valor encapsulado como um tipo primitivo
-
-• boolean compareTo(Object) → que realiza comparações entre objetos das classes wrapper 
-
-
-______________________________________________________________________________________________________
-É preferível usar os métodos fábric valueOf em vez de seus construtores, ou seja, é possível escrever:
-
+// É preferível usar os métodos fábrica valueOf em vez de seus construtores:
 Integer inteiro = Integer.valueOf(4); 
+Double real = Double.valueOf(9.62);
+Boolean logico = Boolean.valueOf("true"); 
+Character caractere = Character.valueOf('a');
 
-Double real Double.valueOf(9.62);
-
-Boolean logico Boolean.valueOf("true"); 
-
-Character caractere Character.valueOf("");
-
-
-________________________________________________________________________________________________
-Para obter o valores encapsulados em objetos wrapper, é possível utilizar os métodos como segue:
-
+// Para obter valores encapsulados em objetos wrapper:
 long l = inteiro.longValue(); 
-
-double d = inteiro.doubleValue();
-
+double d = real.doubleValue();
 float f = real.floatValue(); 
-
 short s = real.shortValue();
-
 boolean b = logico.booleanValue(); 
-
 char c = caractere.charValue();
 
-
-________________________________________________________________________________________________________________________________________________________
-Para simplificar essas operações, foram introduzidos o encaixotamento (autoboxing ou boxing) e desencaixotamento (autounboxing ou unboxing) automáticos:
-
-♦ Encaixotamento automático (autoboxing ou boxing):
-→ permite atribuir valores primitivos para referências do tipo wrapper equivalentes ou mesmo do tipo produzindo a criação de um objeto wrapper:
-
+//_______________________________________________________________________________________________________________________________________
+// Simplificaçao de operações com encaixotamento (autoboxing ou boxing) e desencaixotamento (autounboxing ou unboxing) automáticos:
+// ♦ Encaixotamento automático (autoboxing ou boxing):
+// → permite atribuir valores primitivos para referências do tipo wrapper equivalentes ou mesmo do tipo produzindo a criação de um objeto wrapper:
 Double objD = -2.75; // autoboxing → é equivalente a Double objD = new Double(-2.75);
-
 Object objI= 13; // autoboxing → é equivalente a Object objI = new Integer(13);
 
-A inicialização direta de arranjo também pode empregar o autoboxing: 
-Double valores1[ ] = (-1.35, 0.75, 7.928): // valores são convertidos en Integer Object valores?[] = {0, 1, 2, 3, 4}
+// A inicialização direta de arranjo também pode empregar o autoboxing: 
+Double valores1[] = {-1.35, 0.75, 7.928}; // valores são convertidos em Double
+Integer valores2[] = {0, 1, 2, 3, 4}; // inicialização direta de arranjo
 
 
-♦ Desencaixotamento automático (autounboxing ou unboxing):
-→ De modo semelhante, a atribuição de objetos wrapper a variáveis de tipo primitivo provoca a extração automática de valores entre tipos equivalentes:
-
+// ♦ Desencaixotamento automático (autounboxing ou unboxing):
+// → De modo semelhante, a atribuição de objetos wrapper a variáveis de tipo primitivo provoca a extração automática de valores entre tipos equivalentes:
 double vd = objD; // autounboxing → equivalente a double vd = objD.doubleValue();
+int vi= (int) objI; // autounboxing → equivalente a int vi = objI.intValue();
 
-int vi= objl; // autounboxing → equivalente a int vi= objI.intValue();
-
-O autounboxing também é válido na manipulação de arranjo e seus elementos:
-
+// O autounboxing também é válido na manipulação de arranjo e seus elementos:
 Integer array[] = new Integer [5];  // aloca arranjo de 5 elementos Integer
-
 array[0] = 5; // boxing 
-
-int x = array[0]; // unboxing → atribuição de array[0].intvalue() para vartável x
-
-
-_____________________________________________________________________________________________________________________________
-Para simplificar essas operações, principalmente as relacionadas com a inclusão de valores nas estruturas de dados oferecidas 
-pelas coleções Java (que só aceitam objetos), foram introduzidos o encaixotamento e desencaixotamento automáticos:
-
-
-♦ O encaixotamento automático (autoboxing ou boxing), permite atribuir valores 
-primitivos para referências do tipo wrapper equivalentes ou mesmo do tipo:
-
-Double objD = -2.75; // autoboxing → é equivalente a Double objD = new Double(-2.75);
-
-Object objI= 13; // autoboxing → é equivalente a Object objI = new Integer(13);
-
-Double valores1[ ] = (-1.35, 0.75, 7.928): // autoboxing → inicialização direta de arranjo
- 
-Integer Object valores2[] = {0, 1, 2, 3, 4} // autoboxing → inicialização direta de arranjo
-
-
-♦ No desencaixotamento automático (autounboxing ou unboxing), a atribuição de objetos wrapper a 
-variáveis de tipo primitivo provoca a extração automática de valores entre tipos equivalentes:
-
-double vd = objD; // autounboxing → é quivalente a double vd = objD.doubleValue();
-
-int vi= objl; // autounboxing → é quivalente a int vi= objI.intValue();
-
-Integer array[] = new Integer [5]; // aloca arranjo de 5 elementos Integer
-
-array[0]= 5; // boxing → atribut new Integer (5) à posição e do arranjo
-
-int x = array[0]; // unboxing → atribui array[0].intvalue() para vartável x 
-*/
+int x = array[0]; // unboxing → atribuição de array[0].intValue() para variável x
