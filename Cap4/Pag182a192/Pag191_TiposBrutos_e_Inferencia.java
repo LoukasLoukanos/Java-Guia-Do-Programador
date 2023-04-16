0f9480e68ce2ac027875687966719d43beb6e4b1
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-04-15 22:01:53 -0300

    Pág. 182 à 192

---
+++ Cap4/Pag182a192/Pag191_TiposBrutos_e_Inferencia.java
@@ -0,0 +1,21 @@
+public class Pag191_TiposBrutos_e_Inferencia {
+    /*
+     * • Primeira sintaxe do uso dos Tipos Genéricos:
+     * List<String> lista1 = new ArrayList<String>();
+     * List<Integer> lista2 = new ArrayList<Integer>(); 
+     * Map<String, Integer> mapa1 = HashMap<String, Integer>(); 
+     * Map<Integer, List<String>> mapa2 = new HashMap<Integer, ArrayList<String>>();
+     * 
+     * • Sintaxe disponível a partir do Java 7:
+     * List<String> listal1 = new ArrayListo();
+     * List<Integer> lista2= new ArrayListo();
+     * Map<String, Integer> mapa1 = new HashMap<>();
+     * Map<Integer, List<String>> mapa2 = new HashMapo<>();
+     * 
+     * → O operador <> (diamond) pode ser usado em qualquer situação em que new G<T> seja 
+     * empregado. Aqui G é um tipo genérico que possui parâmetro de tipo e a instância 
+     * produzida é usada num contexto G'<T>, no qual G' é um supertipo ou igual a G, com 
+     * parâmetro de tipo T. Contextos válidos são atribuições, retorno de valores, termos 
+     * de expressões ou mesmo passagem de parâmetros.
+    */
+}
