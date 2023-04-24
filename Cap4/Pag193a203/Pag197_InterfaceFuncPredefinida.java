eb203de21d5fd5e0b73fc554213f475f51835bb2
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-04-24 19:56:17 -0300

    Pág. 193 à 203

--- Cap4/Pag193a203/Pag197_InterfaceFuncPredefinida.java
+++ Cap4/Pag193a203/Pag197_InterfaceFuncPredefinida.java
@@ -29,17 +29,36 @@
 
 import java.util.function.BiFunction;
 import java.util.function.IntToDoubleFunction;
+import java.util.function.DoubleUnaryOperator;
+import java.util.function.IntUnaryOperator;
 
 public class Pag197_InterfaceFuncPredefinida {
 	public static void main(String[] args) {
+
 		// Funcão double (int)
 		IntToDoubleFunction i2df = (l) -> l / (l + 1.0);
-		System.out.println("res = " + i2df.applyAsDouble(4));
-		
+		System.out.println("res = " + i2df.applyAsDouble(4));		
+
 		// Função Double (Double, Double): índice de massa corpórea
 		BiFunction<Double, Double, Double> imc;
 		imc = (m, h) -> m / Math.pow(h, 2);
 		System.out.printf("IMC(m:%.2f|h:%.2f) = %.3f\n",
 				81.5, 1.69, imc.apply(81.5, 1.69));
+
+		// Operador unário int(int) para sucessor: f(n) = n + 1
+		IntUnaryOperator suc = (n) -> n + 1;
+		System.out.println("suc(0) = " + suc.applyAsInt(0));
+		System.out.println("suc(suc(0)) = "
+			+ suc.andThen(suc).applyAsInt(0));
+			
+		// Operadores unários double(double)
+		DoubleUnaryOperator half = (v) -> v / 2;
+		DoubleUnaryOperator plus1 = (v) -> v + 1;
+
+		// Uso em composição anterior e posterior
+		System.out.println("plus1(half(3))) = "
+				+ half.andThen(plus1).applyAsDouble(3));
+		System.out.println("half(plus1(3))) = "
+				+ half.compose(plus1).applyAsDouble(3));
 	}
 }
