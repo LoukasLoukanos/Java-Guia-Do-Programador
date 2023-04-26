0fd0c113854af149a6920921b7102a822a9284ab
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-04-26 19:16:24 -0300

    Pág. 193 à 203

--- Cap4/Pag193a203/Pag197_InterfaceFuncPredefinida.java
+++ Cap4/Pag193a203/Pag197_InterfaceFuncPredefinida.java
@@ -34,6 +34,7 @@ import java.util.function.IntUnaryOperator;
 import java.util.Calendar;
 import java.util.function.IntConsumer;
 import java.util.function.IntSupplier;
+import java.util.function.IntPredicate;
 
 public class Pag197_InterfaceFuncPredefinida {
 	public static void main(String[] args) {
@@ -76,5 +77,23 @@ public class Pag197_InterfaceFuncPredefinida {
 		System.out.print("Consumer::");
 		ref.accept(mes);
 		System.out.println();
+
+		// Predicado: n é potência de dois
+		IntPredicate potDois = (n) -> (Math.log(n) / Math.log(2)) % 2 == 0;
+
+		// Predicado: n > 100
+		IntPredicate cemOuMenos = (n) -> n <= 100;
+
+		// Aplicação separada dos predicados
+		System.out.println("potDois(10): " + potDois.test(10));
+		System.out.println("potDois(1024): " + potDois.test(1024));
+		System.out.println("cemOuMenos(10): " + cemOuMenos.test(10));
+		System.out.println("cemOuMenos(1024): " + cemOuMenos.test(1024));
+
+		// Aplicação combinada dos predicados com encadeamento de métodos
+		System.out.println("potDois(1024) AND cemOuMenos(1024): "
+				+ potDois.and(cemOuMenos).test(1024));
+		System.out.println("cemOuMenos(10) OR potDois(10): "
+				+ cemOuMenos.or(potDois).test(10));
 	}
 }
