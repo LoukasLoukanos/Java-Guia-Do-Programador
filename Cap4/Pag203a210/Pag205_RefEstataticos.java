5476c0ae4840d66d9aea0d34aeb4aadfaf8ff216
Lucas Chagas Ribeiro <eipi1igual0@gmail.com>
2023-05-01 18:27:30 -0300

    Pág. 203 à 210

--- Cap4/Pag203a210/Pag205_RefEstataticos.java
+++ Cap4/Pag203a210/Pag205_RefEstataticos.java
@@ -2,7 +2,7 @@ import java.util.function.*;
 
 public class Pag205_RefEstataticos {
 	public static void main(String[] args) {
-		// Métodos com assinatura double (double)
+		// Métodos estáticos com assinatura double (double)
 		DoubleUnaryOperator f_cbrt = Math::cbrt;
 		DoubleFunction<Double> f_exp = Math::exp;
 		Function<Double, Double> f_log = Math::log;
@@ -10,7 +10,7 @@ public class Pag205_RefEstataticos {
 		System.out.println("exp(1.0)=" + f_exp.apply(1.0));
 		System.out.println("log(64.0)=" + f_log.apply(64.0));
 	
-		// Métodos com assinatura double (double, double)
+		// Métodos estático com assinatura double (double, double)
 		DoubleBinaryOperator f_max = Math::max;
 		BinaryOperator<Double> f_min = Math::min;
 		P0444Calculavel f_pow = Math::pow;
@@ -18,7 +18,7 @@ public class Pag205_RefEstataticos {
 		System.out.println("min(2.5, 5.2)=" + f_min.apply(2.5, 5.2));
 		System.out.println("pow(16.0, 0.5)=" + f_pow.calcular(16.0, 0.5));
 	
-		// Expressões lambda equivalentes
+		// Expressões lambda equivalentes aos métodos estático acima ↑
 		DoubleUnaryOperator f_cbrt2 = v -> Math.cbrt(v);
 		BinaryOperator<Double> f_min2 = (a, b) -> a < b ? a : b;
 		System.out.println("cbrt2(512)=" + f_cbrt2.applyAsDouble(512));
